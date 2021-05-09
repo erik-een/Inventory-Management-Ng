b1e25703317d2bed3ccfb00428002cdb8f2e7370
erik-een <72301766+erik-een@users.noreply.github.com>
2021-05-09 13:39:52 +0300

    removed apiKey from repo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,17 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
- firebaseConfig: {
-  apiKey: 'AIzaSyAAeiDkRDAxZ9LsfZSt80tMc_gScLybe5o',
-  authDomain: 'inventory-management-erikeen.firebaseapp.com',
-  databaseURL:
-    'https://inventory-management-erikeen-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'inventory-management-erikeen',
-  storageBucket: 'inventory-management-erikeen.appspot.com',
-  messagingSenderId: '872607593012',
-  appId: '1:872607593012:web:3fb68ba1a0b92d27a59061',
-  measurementId: 'G-LK0TM9BLDZ',
+  production: false
 }
 
 };
