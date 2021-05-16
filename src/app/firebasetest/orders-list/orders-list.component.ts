4bfe190468443852471a79941d7dd3e67004e4cb
DESKTOP-QD6LVIO\Erik <erik.eenpuu@gmail.com>
2021-05-16 13:27:31 +0300

    firebase data now showing in mat-table

--- src/app/firebasetest/orders-list/orders-list.component.ts
+++ src/app/firebasetest/orders-list/orders-list.component.ts
@@ -1,30 +1,64 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFirestoreDocument } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-import { timeout } from 'rxjs/operators';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
 import { OrdersService } from '../orders-service/orders.service';
-// export interface ICoffeeOrder {
-//   customerName: string;
-//   orderNumber: number;
-//   coffeeOrder: number;
-//   completed: boolean;
-//   payload:
-// }
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Observable } from 'rxjs';
+export interface ICoffeeOrder {
+  customerName: string;
+  orderNumber: number;
+  coffeeOrder: number;
+  completed: boolean;
+}
 @Component({
   selector: 'app-orders-list',
   templateUrl: './orders-list.component.html',
   styleUrls: ['./orders-list.component.scss'],
 })
-export class OrdersListComponent implements OnInit {
+export class OrdersListComponent implements AfterViewInit {
   public coffeeOrders: any[] = [];
-  constructor(private ordersService: OrdersService) {}
+  displayedColumns: string[] = [
+    'customerName',
+    'orderNumber',
+    'coffeeOrder',
+    'completed',
+  ];
 
-  ngOnInit(): void {
+  dataSource: MatTableDataSource<ICoffeeOrder>;
+  private paginator: MatPaginator = null!;
+
+  @ViewChild(MatPaginator) set matPaginator(mp: MatPaginator) {
+    this.paginator = mp;
+    this.dataSource.paginator = this.paginator;
+  }
+  private sort: MatSort = null!;
+
+  @ViewChild(MatSort) set matSort(ms: MatSort) {
+    this.sort = ms;
+    this.dataSource.sort = this.sort;
+  }
+  constructor(private ordersService: OrdersService) {
+    this.dataSource = new MatTableDataSource(this.coffeeOrders);
+  }
+
+  ngAfterViewInit(): void {
     this.getCoffeeOrders();
+  }
+  console() {
+    console.log(this.dataSource);
   }
   getCoffeeOrders() {
     this.ordersService
       .getCoffeeOrders()
       .subscribe((res) => (this.coffeeOrders = res));
+  }
+  applyFilter(event: Event) {
+    console.log(event);
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
--- src/app/firebasetest/orders.module.ts
+++ src/app/firebasetest/orders.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { OrdersComponent } from './orders/orders.component';
 import { OrdersListComponent } from './orders-list/orders-list.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AppMaterialModule } from '../app-material/app-material.module';
 
 @NgModule({
   declarations: [OrdersComponent, OrdersListComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, AppMaterialModule],
   exports: [OrdersListComponent, OrdersComponent],
 })
 export class OrdersModule {}
