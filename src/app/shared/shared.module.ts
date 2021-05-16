032f56d93ddee8002723ef534727cbb84a4080c6
DESKTOP-QD6LVIO\Erik <erik.eenpuu@gmail.com>
2021-05-16 13:56:02 +0300

    adjusted maximum sizes for now

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ButtonComponent } from './buttons/button.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -8,13 +7,8 @@ import { RouterModule } from '@angular/router';
 import { AppMaterialModule } from '../app-material/app-material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
-  declarations: [
-    ButtonComponent,
-    FooterComponent,
-    HeaderComponent,
-    NavbarComponent,
-  ],
+  declarations: [FooterComponent, HeaderComponent, NavbarComponent],
   imports: [CommonModule, RouterModule, AppMaterialModule, FlexLayoutModule],
-  exports: [ButtonComponent, FooterComponent, HeaderComponent, NavbarComponent],
+  exports: [FooterComponent, HeaderComponent, NavbarComponent],
 })
 export class SharedModule {}
